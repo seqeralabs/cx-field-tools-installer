75947ab2dbc04a8e86b8c61dcdd724d090b28f3c
mppan821 <mark.panganiban@seqera.io>
2025-05-06 10:58:25 -0400

    Fixed typo

--- scripts/installer/validation/check_configuration.py
+++ scripts/installer/validation/check_configuration.py
@@ -200,7 +200,7 @@ def verify_docker_daemon_loggin(data: SimpleNamespace):
 
 
 def verify_email_login_disablement(data: SimpleNamespace):
-    """Check workflow cleanup enablement scenarios."""
+    """Check email login disablement scenarios."""
     if data.flag_disable_email_login:
 
         if data.tower_container_version < "v23.4.5":
@@ -224,7 +224,7 @@ def verify_email_login_disablement(data: SimpleNamespace):
             )
 
 def verify_workflow_cleanup_enabled(data: SimpleNamespace):
-    """Check email login disablement scenarios."""
+    """Check workflow cleanup enablement scenarios."""
     if data.tower_workflow_cleanup_enabled:
 
         if data.tower_container_version < "v25.1.0":
