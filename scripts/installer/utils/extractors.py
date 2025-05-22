1caa162e4ce1e77140962935176b3cad2618f532
Graham Wright <graham.wright@seqera.io>
2025-05-22 08:07:55 -0400

    Added architecture check in extractors.py to seamlessly swap between amd64 and arm64 docker types.

--- scripts/installer/utils/extractors.py
+++ scripts/installer/utils/extractors.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 from pathlib import Path
+import platform
 import subprocess
 import sys
 import tempfile
@@ -40,18 +41,26 @@ def get_tfvars_as_json():
         raise FileNotFoundError("terraform.tfvars file not found in current directory.")
 
     try:
-        # Create a temporary file for Docker output
-        # delete=True would delete the tmp file as soon as the `with` block exits but we need the file further down
-        with tempfile.NamedTemporaryFile(delete=False, prefix=f"terraform_tfvars_{timestamp}_", suffix=".json", dir="/tmp") as temp_output:
-            output_path = temp_output.name
+        # Deployment machines are a mix of Intel and Mac. Dynamically assess chip architecture to pass correct
+        # container architecture qualifier.
+        arch = platform.machine()
+        if (arch == "x86_64") or (arch == "AMD64"):
+            arch = "amd64"
+        elif (arch == "arm64"):
+            arch = "arm64"
+        else:
+            raise Exception("Chip-type is not x86_64, AMD64, or arm64.")
 
+        # Because this is a 3rd party container, we are locking down as much as possible for security reasons:
+        # Single local file mounted as read-only, non-root user, disabled network capabilities, and
+        # use of immutable container hash fingerprint over mutable tag.
         cmd = [
                 "docker", "run",
-                "--platform", "linux/amd64",
+                "--platform", f"linux/{arch}",
                 "-i", "--rm",
-                "-v", f"{os.getcwd()}:/tmp:ro",            # read-only mount
-                "--user", "1000:1000",                     # non-root user (adjust UID/GID if needed or user current user)
-                "--network", "none",                       # disable network
+                "-v", f"{os.getcwd()}/terraform.tfvars:/tmp/terraform.tfvars:ro",
+                "--user", "1000:1000",
+                "--network", "none",
                 "tmccombs/hcl2json@sha256:312ac54d3418b87b2ad64f82027483cb8b7750db6458b7b9ebe42ec278696e96",
                 "/tmp/terraform.tfvars"
             ]
@@ -61,24 +70,15 @@ def get_tfvars_as_json():
         payload = result.stdout
 
         # Redirect output to temporary JSON file (debugging only)
-        if logging.getLevelName(logger.getEffectiveLevel()):
-            with open(output_path, "w") as outfile:
-                outfile.writelines(payload)
-            # with open(output_path, "w") as outfile:
-            #     subprocess.run(cmd, stdout=outfile, stderr=subprocess.PIPE, check=True)
+        if logging.getLevelName(logger.getEffectiveLevel()) == "DEBUG":
+            with tempfile.NamedTemporaryFile(delete=False, prefix=f"terraform_tfvars_{timestamp}_", suffix=".json", mode='w+', dir="/tmp") as temp_output:
+                temp_output.write(payload)
 
         logger.info(payload)
         return json.loads(payload)
 
     except subprocess.CalledProcessError as e:
         raise RuntimeError(f"Docker command failed: {e.stderr.decode().strip()}")
-
-    # # Read and return the parsed JSON
-    # with open(output_path, "r") as json_file:
-    #     return json.load(json_file)
-    
-    # TODO: Should script clean up after using the json
-    # os.remove(output_path)
 
 
 tf_vars_json_payload = get_tfvars_as_json()
