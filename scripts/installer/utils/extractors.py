1f26e226f8ee32c6ad559e94ccc49caeee5f3c88
Sushma Chaluvadi <43828038+schaluva@users.noreply.github.com>
2025-05-22 15:35:07 -0400

    Merge changes to extractors.py into original schaluva/hcl-json-docker-parser with gwright/add-wave-lite changes (#208)
    * update docker pull with sha, add note about tempfile params
    * removing architecture if and hardcoding into cmd, moved timestamp

--- scripts/installer/utils/extractors.py
+++ scripts/installer/utils/extractors.py
@@ -8,7 +8,6 @@ import subprocess
 import sys
 import tempfile
 
-
 base_import_dir = Path(__file__).resolve().parents[2]
 if base_import_dir not in sys.path:
     sys.path.append(str(base_import_dir))
@@ -34,51 +33,46 @@ def get_tfvars_as_json():
     a Python dictionary.
     """
     
+    # Check for tfvars
     tfvars_path = os.path.abspath("terraform.tfvars")
-    timestamp = datetime.now().strftime("%Y_%b%d_%I-%M%p")
-    
     if not os.path.exists(tfvars_path):
         raise FileNotFoundError("terraform.tfvars file not found in current directory.")
 
+    # Because this is a 3rd party container, we are locking down as much as possible for security reasons:
+    # Single local file mounted as read-only, non-root user, disabled network capabilities, and
+    # use of immutable container hash fingerprint over mutable tag.
+    cmd = [
+            "docker", "run",
+            "--platform", "linux/amd64",
+            "-i", "--rm",
+            "-v", f"{os.getcwd()}/terraform.tfvars:/tmp/terraform.tfvars:ro",
+            "--user", "1000:1000",
+            "--network", "none",
+            "tmccombs/hcl2json@sha256:312ac54d3418b87b2ad64f82027483cb8b7750db6458b7b9ebe42ec278696e96",
+            "/tmp/terraform.tfvars"
+        ]
+    
+    # Assign result to variable (to then be returned directly or written to file for debugging)
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    
+    # Capture Docker runtime failures
+    if result.returncode != 0:
+        raise RuntimeError(f"Docker command failed:\nSTDERR: {result.stderr.strip()}")
+
+    # Capture Docker stdout
+    payload = result.stdout.strip()
+
+    # Redirect output to temporary JSON file (debugging only)
+    if logging.getLevelName(logger.getEffectiveLevel()) == "DEBUG":
+        timestamp = datetime.now().strftime("%Y_%b%d_%I-%M%p")
+        with tempfile.NamedTemporaryFile(delete=False, prefix=f"terraform_tfvars_{timestamp}_", suffix=".json", mode='w+', dir="/tmp") as temp_output:
+            temp_output.write(payload)
+    
+    # Capture invalid json errors
     try:
-        # Deployment machines are a mix of Intel and Mac. Dynamically assess chip architecture to pass correct
-        # container architecture qualifier.
-        arch = platform.machine()
-        if (arch == "x86_64") or (arch == "AMD64"):
-            arch = "amd64"
-        elif (arch == "arm64"):
-            arch = "arm64"
-        else:
-            raise Exception("Chip-type is not x86_64, AMD64, or arm64.")
-
-        # Because this is a 3rd party container, we are locking down as much as possible for security reasons:
-        # Single local file mounted as read-only, non-root user, disabled network capabilities, and
-        # use of immutable container hash fingerprint over mutable tag.
-        cmd = [
-                "docker", "run",
-                "--platform", f"linux/{arch}",
-                "-i", "--rm",
-                "-v", f"{os.getcwd()}/terraform.tfvars:/tmp/terraform.tfvars:ro",
-                "--user", "1000:1000",
-                "--network", "none",
-                "tmccombs/hcl2json@sha256:312ac54d3418b87b2ad64f82027483cb8b7750db6458b7b9ebe42ec278696e96",
-                "/tmp/terraform.tfvars"
-            ]
-
-        # Assign result to variable (to then be returned directly or written to file for debugging)
-        result = subprocess.run(cmd, capture_output=True, text=True)
-        payload = result.stdout
-
-        # Redirect output to temporary JSON file (debugging only)
-        if logging.getLevelName(logger.getEffectiveLevel()) == "DEBUG":
-            with tempfile.NamedTemporaryFile(delete=False, prefix=f"terraform_tfvars_{timestamp}_", suffix=".json", mode='w+', dir="/tmp") as temp_output:
-                temp_output.write(payload)
-
         logger.info(payload)
         return json.loads(payload)
-
-    except subprocess.CalledProcessError as e:
-        raise RuntimeError(f"Docker command failed: {e.stderr.decode().strip()}")
-
+    except json.JSONDecodeError as e:
+        raise RuntimeError("Failed to decode Docker output as JSON.") from e
 
 tf_vars_json_payload = get_tfvars_as_json()
