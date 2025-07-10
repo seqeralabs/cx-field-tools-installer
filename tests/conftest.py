4fee36bdae9a06918d3b7017fb4f478068c974be
Graham Wright <graham.wright@seqera.io>
2025-07-10 15:39:56 -0400

    Checkpoint: Moved config_file generation logic to fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,7 +17,8 @@ from tests.utils.local import test_tfvars_override_source, test_tfvars_override_
 from tests.utils.local import test_case_override_target
 
 from tests.utils.local import copy_file, move_file
-from tests.utils.local import run_terraform_destroy
+from tests.utils.local import prepare_plan
+from tests.utils.local import run_terraform_apply, run_terraform_destroy
 
 
 """
@@ -69,6 +70,47 @@ def backup_tfvars():
 
     delete_pycache_folders(root)
     move_file(tfvars_backup_path, tfvars_path)
+
+
+# TODO: Make sure module is for the file calling it, NOT where this fixture lives.
+@pytest.fixture(scope="module")
+def config_baseline_settings_default():
+    """
+    Terraform plan and apply the default test terraform.tfvars and base-override.auto.tfvars.
+
+    This test is slower than test_module_connections_strings since we need to plan every time and apply.
+    However, it generates almost every configuration (Tower / Wave Lite / Ansible / etc) so it's worth the time to check.
+    I use an existing VPC in the account the AWS provider is configured to use to speed things up, but it's not perfect.
+    TODO: Figure out if there's some way to speed up the plan (i.e. split out `data` resource calls? As of July 9/25, resources created:
+      - data.aws_ssm_parameter.groundswell_secrets
+      - data.aws_ssm_parameter.tower_secrets
+      - data.aws_ssm_parameter.wave_lite_secrets
+      - null_resource.generate_independent_config_files
+      - random_pet.stackname
+      - tls_private_key.connect_pem
+      - tls_private_key.ec2_ssh_key
+      - module.connection_strings.data.external.generate_db_connection_string
+      - module.subnet_collector.data.aws_subnet.sp_subnets["subnet-00fad764627895f33"]
+      - module.subnet_collector.data.aws_subnet.sp_subnets["subnet-0d0e8ba3b03b97a65"]
+      - module.subnet_collector.data.aws_subnet.sp_subnets["subnet-0e07c9b2edbd84ea4"]
+      - module.subnet_collector.data.aws_subnet.sp_subnets["subnet-0f18039d5ffcf6cd3"]
+      - module.subnet_collector.data.aws_subnets.all[0]
+      - module.subnet_collector.data.aws_vpc.sp_vpc)
+    """
+
+    override_data = """
+        # No override values needed. Testing baseline only.
+    """
+
+    qualifier = "-target=null_resource.generate_independent_config_files"
+    plan = prepare_plan(override_data, qualifier)
+    run_terraform_apply(qualifier)
+
+    # Apply will generate actual assets we can interrogate in project or via remote calls.
+    # Return plan only
+    yield plan
+
+    run_terraform_destroy()
 
 
 @pytest.fixture(scope="function")
--- tests/unit/config_files/test_tower_config_files.py
+++ tests/unit/config_files/test_tower_config_files.py
@@ -15,39 +15,19 @@ from tests.utils.local import parse_key_value_file
 @pytest.mark.local
 @pytest.mark.config_files
 @pytest.mark.vpc_existing
-def test_config_baseline_settings_quick(backup_tfvars, teardown_tf_state_all):
+@pytest.mark.quick
+def test_config_baseline_settings_default(
+    backup_tfvars, config_baseline_settings_default
+):  # teardown_tf_state_all):
     """
-    This test is slower than test_module_connections_strings since we need to plan every time and apply.
-    However, it generates almost every configuration (Tower / Wave Lite / Ansible / etc) so it's worth the time to check.
-    I use an existing VPC in the account the AWS provider is configured to use to speed things up, but it's not perfect.
-    TODO: Figure out if there's some way to speed up the plan (i.e. split out `data` resource calls? As of July 9/25, resources created:
-      - data.aws_ssm_parameter.groundswell_secrets
-      - data.aws_ssm_parameter.tower_secrets
-      - data.aws_ssm_parameter.wave_lite_secrets
-      - null_resource.generate_independent_config_files
-      - random_pet.stackname
-      - tls_private_key.connect_pem
-      - tls_private_key.ec2_ssh_key
-      - module.connection_strings.data.external.generate_db_connection_string
-      - module.subnet_collector.data.aws_subnet.sp_subnets["subnet-00fad764627895f33"]
-      - module.subnet_collector.data.aws_subnet.sp_subnets["subnet-0d0e8ba3b03b97a65"]
-      - module.subnet_collector.data.aws_subnet.sp_subnets["subnet-0e07c9b2edbd84ea4"]
-      - module.subnet_collector.data.aws_subnet.sp_subnets["subnet-0f18039d5ffcf6cd3"]
-      - module.subnet_collector.data.aws_subnets.all[0]
-      - module.subnet_collector.data.aws_vpc.sp_vpc)
+    Test the target folder generated from default test terraform.tfvars and base-override.auto.tfvars.
     """
 
     # Given
-    override_data = """
-        # No override values needed. Testing baseline only.
-    """
+    print("Testing target/ folder generated from default settings.")
 
     # When
-    # outputs = plan["planned_values"]["outputs"]
-
-    qualifier = "-target=null_resource.generate_independent_config_files"
-    plan = prepare_plan(override_data, qualifier)
-    run_terraform_apply(qualifier)
+    outputs = config_baseline_settings_default["planned_values"]["outputs"]
     # execute_subprocess(f"terraform state list > terraform_state_list.txt")
 
     # Crack actual created file
@@ -71,10 +51,7 @@ def test_config_baseline_settings_quick(backup_tfvars, teardown_tf_state_all):
     assert "TOWER_DB_MAX_LIFETIME" in file
     assert "FLYWAY_LOCATIONS" in file
     assert "TOWER_REDIS_URL" in file
-    assert "TOWER_SMTP_HOST" in file
-    assert "TOWER_SMTP_PORT" in file
     assert "WAVE_SERVER_URL" in file
-    assert "GROUNDSWELL_SERVER_URL" in file
     assert "TOWER_DATA_EXPLORER_CLOUD_DISABLED_WORKSPACES" in file
 
     # Test tower.env - assert some core keys NOT present
@@ -94,10 +71,13 @@ def test_config_baseline_settings_quick(backup_tfvars, teardown_tf_state_all):
     assert "TOWER_DATA_EXPLORER_ENABLED" in file
 
     # Test sometimes-present conditionals
-    assert "TOWER_DATA_STUDIO_ALLOWED_WORKSPACES" in file
-    assert "TOWER_DATA_STUDIO_CONNECT_URL" in file
-    assert "TOWER_OIDC_PEM_PATH" in file
-    assert "TOWER_OIDC_REGISTRATION_INITIAL_ACCESS_TOKEN" in file
+    # assert "TOWER_SMTP_HOST" in file
+    # assert "TOWER_SMTP_PORT" in file
+    # assert "GROUNDSWELL_SERVER_URL" in file
+    # assert "TOWER_DATA_STUDIO_ALLOWED_WORKSPACES" in file
+    # assert "TOWER_DATA_STUDIO_CONNECT_URL" in file
+    # assert "TOWER_OIDC_PEM_PATH" in file
+    # assert "TOWER_OIDC_REGISTRATION_INITIAL_ACCESS_TOKEN" in file
 
     # assert "TOWER_DATA_STUDIO_TEMPLATES_${ds.qualifier}_ICON" in file
     # assert "TOWER_DATA_STUDIO_TEMPLATES_${ds.qualifier}_REPOSITORY" in file
