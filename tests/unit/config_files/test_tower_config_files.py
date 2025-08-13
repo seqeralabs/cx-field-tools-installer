361e9295c5087b58566aec2728e6994c43c00623
Graham Wright <graham.wright@seqera.io>
2025-08-13 08:21:00 -0400

    CHECKPOINT: Moved remaining db tests (db existing) into test_tower_config_files.py

--- tests/unit/config_files/test_tower_config_files.py
+++ tests/unit/config_files/test_tower_config_files.py
@@ -345,10 +345,144 @@ def test_new_db_all_disabled(session_setup):
         "omitted": {}
     }
 
+    # No need for custom overrides -- core testcase handles this one.
+    overrides["groundswell_env"]= {
+        "present": {},
+        "omitted": {}
+    }
+
+    # No need for custom overrides -- core testcase handles this one.
+    overrides["wave_lite_yml"]= {
+        "present": {},
+        "omitted": {}
+    }
+    baseline_all_entries = generate_baseline_entries_all_disabled(test_template_files, overrides)
+
+    # ------------------------------------------------------------------------------------
+    # Test files
+    # ------------------------------------------------------------------------------------
+    keys = file_targets_all
+    
+    for key, type in keys.items():
+        print(f"Testing {sys._getframe().f_code.co_name}.{key} generated from default settings.")
+        file = test_template_files[key]["content"]
+        entries = baseline_all_entries[key]
+        assert_present_and_omitted(entries, file, type)
+
+
+## ------------------------------------------------------------------------------------
+## MARK: Existing DB: All Active
+## ------------------------------------------------------------------------------------
+@pytest.mark.local
+@pytest.mark.container
+@pytest.mark.db_existing
+def test_existing_db_all_enabled(session_setup):
+    """
+    Baseline check of configuration.
+        - All services enabled (Wave Lite, Groundswell, Data Explorer, Studios)
+        - Use of machine identity whenever possible (SES).
+        - Use of HTTPS
+        - Use of container Redis & DB
+        - TODO: No OIDC (have not figured out how to configure in public yet -- maybe use pre-generated environment variables?)
+    """
+
+    override_data = """
+        flag_create_external_db         = false
+        flag_use_existing_external_db   = true
+        flag_use_container_db           = false
+        tower_db_url                        = "existing.tower-db.com"
+    """
+    # Plan with ALL resources rather than targeted, to get all outputs in plan document.
+    plan = prepare_plan(override_data)
+
+    needed_template_files = all_template_files
+    test_template_files = set_up_testcase(plan, needed_template_files, sys._getframe().f_code.co_name)
+
+    overrides = deepcopy(overrides_template)
+    overrides["tower_env"]= {
+        "present": {
+            "TOWER_DB_URL"                : "jdbc:mysql://existing.tower-db.com:3306/tower?allowPublicKeyRetrieval=true&useSSL=false&permitMysqlScheme=true",
+        },
+        "omitted": {}
+    }
+
     overrides["groundswell_env"]= {
         "present": {
-            "TOWER_DB_URL"                : "jdbc:mysql://mock.tower-db.com:3306/tower?allowPublicKeyRetrieval=true&useSSL=false&permitMysqlScheme=true",
+            "TOWER_DB_URL"                : "jdbc:mysql://existing.tower-db.com:3306/tower?allowPublicKeyRetrieval=true&useSSL=false&permitMysqlScheme=true",
         },
+        "omitted": {}
+    }
+
+    # NOTE: Current as of Aug 13/2025, Wave-Lite does not support existing db flow.
+    # TODO: Extend functionality.
+    overrides["wave_lite_yml"]= {
+        "present": {
+            'wave.db.uri'                 : "jdbc:postgresql://wave-db:5432/wave",
+        },
+        "omitted": {}
+    }
+
+    baseline_all_entries = generate_baseline_entries_all_active(test_template_files, overrides)
+
+    # ------------------------------------------------------------------------------------
+    # Test files
+    # ------------------------------------------------------------------------------------
+    keys = file_targets_all
+    
+    for key, type in keys.items():
+        print(f"Testing {sys._getframe().f_code.co_name}.{key} generated from default settings.")
+        file = test_template_files[key]["content"]
+        entries = baseline_all_entries[key]
+        assert_present_and_omitted(entries, file, type)
+
+
+## ------------------------------------------------------------------------------------
+## MARK: New DB: All Disabled
+## ------------------------------------------------------------------------------------
+@pytest.mark.local
+@pytest.mark.container
+@pytest.mark.db_existing
+def test_existing_db_all_disabled(session_setup):
+    """
+    Baseline check of configuration.
+        - All services disabled (Wave Lite, Groundswell, Data Explorer, Studios)
+        - No machine identity (SES).
+        - Use of HTTPS
+        - Use of container Redis & DB
+        - TODO: No OIDC (have not figured out how to configure in public yet -- maybe use pre-generated environment variables?)
+    """
+
+    override_data = """
+        flag_use_aws_ses_iam_integration    = false
+        flag_use_existing_smtp              = true
+        flag_enable_groundswell             = false
+        flag_data_explorer_enabled          = false
+        flag_enable_data_studio             = false
+        flag_use_wave                       = false
+        flag_use_wave_lite                  = false
+
+        flag_create_external_db             = false
+        flag_use_existing_external_db       = true
+        flag_use_container_db               = false
+        tower_db_url                        = "existing.tower-db.com"
+    """
+    # Plan with ALL resources rather than targeted, to get all outputs in plan document.
+    plan = prepare_plan(override_data)
+
+    needed_template_files = all_template_files
+    test_template_files = set_up_testcase(plan, needed_template_files, sys._getframe().f_code.co_name)
+
+    overrides = deepcopy(overrides_template)
+    overrides["tower_env"]= {
+        "present": {
+            "TOWER_DB_URL"                : "jdbc:mysql://existing.tower-db.com:3306/tower?allowPublicKeyRetrieval=true&useSSL=false&permitMysqlScheme=true",
+        },
+        "omitted": {}
+    }
+
+    # No need for custom overrides -- core testcase handles this one.
+    overrides["groundswell_env"]= {
+        "present": {},
         "omitted": {}
     }
 
