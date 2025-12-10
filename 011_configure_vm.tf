6d192e4ff372e2c7df2c5d222bdcfea503c6d99d
Graham Wright <graham.wright@seqera.io>
2025-12-09 22:49:14 -0500

    chore: suppress various terraform console log noise
    See: https://github.com/seqeralabs/cx-field-tools-installer/issues/288

--- 011_configure_vm.tf
+++ 011_configure_vm.tf
@@ -17,12 +17,13 @@ resource "null_resource" "ssh_connectivity_check" {
   depends_on = [null_resource.allow_file_copy_to_start]
 
   provisioner "local-exec" {
+    quiet       = true
     command     = <<-EOT
       set -e
       echo "[$(date)] Starting SSH connectivity check for ${var.app_name}"
       
       counter=0
-      until ssh ${var.app_name} || [ $counter -gt 60 ]; do
+      until ssh -T ${var.app_name} || [ $counter -gt 60 ]; do
         echo "[$(date)] Waiting for SSH connection to be available."
         sleep 5
         counter=$((counter+5))
@@ -44,11 +45,12 @@ resource "null_resource" "file_transfer" {
   depends_on = [null_resource.ssh_connectivity_check]
 
   provisioner "local-exec" {
+    quiet       = true
     command     = <<-EOT
       set -e
       echo "[$(date)] Starting file transfer to ${var.app_name}"
       echo "[$(date)] Purging old target folder on remote VM"
-      ssh ${var.app_name} 'cd /home/ec2-user && rm -rf target || true'
+      ssh -T ${var.app_name} 'cd /home/ec2-user && rm -rf target || true'
 
       echo "[$(date)] Transferring new target folder"
       scp -r assets/target ${var.app_name}:/home/ec2-user/target
@@ -68,10 +70,11 @@ resource "null_resource" "host_configuration" {
   depends_on = [null_resource.file_transfer]
 
   provisioner "local-exec" {
+    quiet       = true
     command     = <<-EOT
       set -e
       echo "[$(date)] Starting host configuration for ${var.app_name}"
-      ssh ${var.app_name} '/bin/bash /home/ec2-user/target/bash/remote/cleanse_and_configure_host.sh'
+      ssh -T ${var.app_name} '/bin/bash /home/ec2-user/target/bash/remote/cleanse_and_configure_host.sh'
       echo "[$(date)] Host configuration completed successfully"
     EOT
     interpreter = ["/bin/bash", "-c"]
@@ -88,10 +91,11 @@ resource "null_resource" "ansible_setup" {
   depends_on = [null_resource.host_configuration]
 
   provisioner "local-exec" {
+    quiet       = true
     command     = <<-EOT
       set -e
       echo "[$(date)] Waiting for Ansible to be ready on ${var.app_name}"
-      ssh ${var.app_name} 'cd ${local.playbook_dir} && chmod u+x 00_wait_for_ansible.sh && ./00_wait_for_ansible.sh'
+      ssh -T ${var.app_name} 'cd ${local.playbook_dir} && chmod u+x 00_wait_for_ansible.sh && ./00_wait_for_ansible.sh'
       echo "[$(date)] Ansible setup completed successfully"
     EOT
     interpreter = ["/bin/bash", "-c"]
@@ -108,10 +112,11 @@ resource "null_resource" "system_packages" {
   depends_on = [null_resource.ansible_setup]
 
   provisioner "local-exec" {
+    quiet       = true
     command     = <<-EOT
       set -e
       echo "[$(date)] Loading System Packages on ${var.app_name}"
-      ssh ${var.app_name} 'set -e && cd ${local.playbook_dir} && ansible-playbook -i inventory.ini 01_load_system_packages.yml'
+      ssh -T ${var.app_name} 'set -e && cd ${local.playbook_dir} && ansible-playbook -i inventory.ini 01_load_system_packages.yml'
       echo "[$(date)] System packages installation completed successfully"
     EOT
     interpreter = ["/bin/bash", "-c"]
@@ -128,10 +133,11 @@ resource "null_resource" "update_configuration_files" {
   depends_on = [null_resource.system_packages]
 
   provisioner "local-exec" {
+    quiet       = true
     command     = <<-EOT
       set -e
       echo "[$(date)] Updating Configuration Files on ${var.app_name}"
-      ssh ${var.app_name} 'cd ${local.playbook_dir} && ansible-playbook -i inventory.ini  02_update_file_configurations.yml'
+      ssh -T ${var.app_name} 'cd ${local.playbook_dir} && ansible-playbook -i inventory.ini  02_update_file_configurations.yml'
       echo "[$(date)] Configuration files updated successfully"
     EOT
     interpreter = ["/bin/bash", "-c"]
@@ -148,10 +154,11 @@ resource "null_resource" "pull_containers_run_tower" {
   depends_on = [null_resource.update_configuration_files]
 
   provisioner "local-exec" {
+    quiet       = true
     command     = <<-EOT
       set -e
       echo "[$(date)] Pulling containers and running Tower on ${var.app_name}"
-      ssh ${var.app_name} 'cd ${local.playbook_dir} && ansible-playbook -i inventory.ini 03_pull_containers_and_run_tower.yml'
+      ssh -T ${var.app_name} 'cd ${local.playbook_dir} && ansible-playbook -i inventory.ini 03_pull_containers_and_run_tower.yml'
       echo "[$(date)] Containers pulled and Tower started successfully"
     EOT
     interpreter = ["/bin/bash", "-c"]
@@ -168,10 +175,11 @@ resource "null_resource" "wait_for_tower" {
   depends_on = [null_resource.pull_containers_run_tower]
 
   provisioner "local-exec" {
+    quiet       = true
     command     = <<-EOT
       set -e
       echo "[$(date)] Waiting for Tower containers to be ready on ${var.app_name}"
-      ssh ${var.app_name} 'cd ${local.playbook_dir} && ansible-playbook -i inventory.ini 04_wait_for_tower.yml'
+      ssh -T ${var.app_name} 'cd ${local.playbook_dir} && ansible-playbook -i inventory.ini 04_wait_for_tower.yml'
       echo "[$(date)] Tower containers are ready successfully"
     EOT
     interpreter = ["/bin/bash", "-c"]
@@ -188,10 +196,11 @@ resource "null_resource" "patch_groundswell" {
   depends_on = [null_resource.wait_for_tower]
 
   provisioner "local-exec" {
+    quiet       = true
     command     = <<-EOT
       set -e
       echo "[$(date)] Patching Groundswell (if necessary) on ${var.app_name}"
-      ssh ${var.app_name} 'cd ${local.playbook_dir} && ansible-playbook -i inventory.ini  05_patch_groundswell.yml'
+      ssh -T ${var.app_name} 'cd ${local.playbook_dir} && ansible-playbook -i inventory.ini  05_patch_groundswell.yml'
       echo "[$(date)] Groundswell patching completed successfully"
     EOT
     interpreter = ["/bin/bash", "-c"]
@@ -209,13 +218,11 @@ resource "null_resource" "run_seqerkit" {
   depends_on = [null_resource.patch_groundswell]
 
   provisioner "local-exec" {
+    quiet       = true
     command     = <<-EOT
-
       set -e
-
       echo "Running Seqerakit"
-      ssh ${var.app_name} 'cd ${local.playbook_dir} && ansible-playbook -i inventory.ini  06_run_seqerakit.yml'
-
+      ssh -T ${var.app_name} 'cd ${local.playbook_dir} && ansible-playbook -i inventory.ini  06_run_seqerakit.yml'
     EOT
     interpreter = ["/bin/bash", "-c"]
   }
